8b8c875cd1e8fdfc40968a9d33b1b30275a00284
Mathieu Duprez <duprez.math@gmail.com>
2018-12-02 06:31:33 +0100

    biip

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,13 +7,4 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-
 console.log('Hello World from Webpacker')
-import "bootstrap";
-import "customejs";
-import "jquery";
-import 'plusminus';
-import 'magnifier-rails';
-
-
-
