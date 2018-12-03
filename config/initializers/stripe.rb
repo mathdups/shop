4b8927e0fac81f74c8ff02536171e8f2a0cfe070
Mathieu Duprez <duprez.math@gmail.com>
2018-12-03 02:19:22 +0100

    ..

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -5,3 +5,9 @@ Rails.configuration.stripe = {
   secret_key: Rails.application.secrets.stripe_secret_key
 }
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
+
+
+Rails.configuration.stripe = {
+  :publishable_key => ENV['PUBLISHABLE_KEY'],
+  :secret_key      => ENV['SECRET_KEY']
+}
