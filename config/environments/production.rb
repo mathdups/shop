fae2a2fc8e642c2c38ebba0b0e0abfc579aea936
Mathieu Duprez <duprez.math@gmail.com>
2018-12-05 04:04:02 +0100

    ..

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,9 @@ config.webpacker.check_yarn_integrity = false
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "shopper_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { :host => 'https://obscure-castle-96967.herokuapp.com' }
+
+  
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
